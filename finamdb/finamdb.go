dc470f79c88d6608f5006ebc316a51f320437a8a
Evgeny Voytik <vvea@mail.ru>
2022-03-02 17:25:21 +0300

    mssql connection
    (cherry picked from commit 02e9c6b3277a1a584ec944a2add62474e161ddde)

--- finamdb/finamdb.go
+++ finamdb/finamdb.go
@@ -2,17 +2,18 @@ package finamdb
 
 import (
 	"database/sql"
+	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type FinamUserData struct {
-	Name  string
+	Fname string
+	Lname string
 	Email string
 	Phone string
 }
 
 type dbCredentials struct {
-	dbType   string
 	host     string
 	port     string
 	user     string
@@ -20,8 +21,7 @@ type dbCredentials struct {
 	dbName   string
 }
 
-var dbCred dbCredentials = dbCredentials{
-	dbType:   "mysql",
+var dbCredMysql dbCredentials = dbCredentials{
 	host:     "10.0.3.11",
 	port:     "3306",
 	user:     "webuser",
@@ -29,19 +29,44 @@ var dbCred dbCredentials = dbCredentials{
 	dbName:   "finam_auth",
 }
 
+var dbCredMssql dbCredentials = dbCredentials{
+	host:     "127.0.0.1",
+	port:     "1433",
+	user:     "sa",
+	password: "Juurfj48TGu",
+	dbName:   "master",
+}
+
 const ()
 
-func GetFinamUserData(login string, password string) (FinamUserData, error) {
+func GetFinamUserDataMysql(login string, password string) (FinamUserData, error) {
 
 	var userData FinamUserData
-	dsn := dbCred.user + ":" + dbCred.password + "@tcp(" + dbCred.host + ":" + dbCred.port + ")/" + dbCred.dbName
-	dbConn, err := sql.Open(dbCred.dbType, dsn)
+	dsn := dbCredMysql.user + ":" + dbCredMysql.password + "@tcp(" + dbCredMysql.host + ":" + dbCredMysql.port + ")/" + dbCredMysql.dbName
+	dbConn, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return userData, err
 	}
 
-	err = dbConn.QueryRow("CALL getuser(?, ?)", login, password).Scan(&userData.Name, &userData.Phone, &userData.Email)
+	err = dbConn.QueryRow("CALL getuser(?, ?)", login, password).Scan(&userData.Fname, &userData.Lname, &userData.Phone, &userData.Email)
 
+	err = dbConn.Close()
+	if err != nil {
+		return FinamUserData{}, err
+	}
+
+	return userData, err
+}
+
+func GetFinamUserDataMssql(login string, password string) (FinamUserData, error) {
+	var userData FinamUserData
+	dsn := "sqlserver://" + dbCredMssql.user + ":" + dbCredMssql.password + "@" + dbCredMssql.host + ":" + dbCredMssql.port + "?database=" + dbCredMssql.dbName
+
+	dbConn, err := sql.Open("sqlserver", dsn)
+	if err != nil {
+		return userData, err
+	}
+	err = dbConn.QueryRow("exec getuser @inlogin='?', @inpass='?'", login, password).Scan(&userData.Fname, &userData.Lname, &userData.Phone, &userData.Email)
 	err = dbConn.Close()
 	if err != nil {
 		return FinamUserData{}, err
